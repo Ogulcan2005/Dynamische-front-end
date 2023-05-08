60bdf8e38ddf5be1dc494c8ed4fde64c86acd703
ogulcan dinc <you@example.com>
2023-05-08 13:36:26 +0200

    ''

--- Module 2/button03/button03.js
+++ Module 2/button03/button03.js
@@ -2,7 +2,7 @@ const buttons = document.querySelectorAll("button");
 const aantal_kliks = Array.from(buttons, () => 0);
 const colors = Array('red', 'purple', 'blue', 'black', 'brown', 'yellow', 'orange')
 console.log(aantal_kliks)
-for (let number_button = 0; number_button < buttons.length; i++) {
+for (let number_button = 0; number_button < buttons.length; number_button++) {
     buttons[number_button].addEventListener("click", function () {
         if (aantal_kliks[number_button] < colors.length){
             this.style.backgroundColor = colors[aantal_kliks[number_button]];
