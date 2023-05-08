b4c2c7296c609ebf8f25af00dfd36667da18b3e0
ogulcan dinc <you@example.com>
2023-05-08 13:28:13 +0200

    ''

--- Module 2/button03/button03.js
+++ Module 2/button03/button03.js
@@ -1,30 +1,19 @@
 const buttons = document.querySelectorAll("button");
 const aantal_kliks = Array.from(buttons, () => 0);
-
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        aantal_kliks[i]++;
-        switch (aantal_kliks[i]) {
-            case 1:
-                this.style.backgroundColor = "red";
-                break;
-            case 2:
-                this.style.backgroundColor = "purple";
-                break;
-            case 3:
-                this.style.backgroundColor = "blue";
-                break;
-            case 4:
-                this.style.backgroundColor = "black";
-                break;
-            case 5:
-                this.style.display = "none";
-                break;
+const colors = Array('red', 'purple', 'blue', 'black', 'brown', 'yellow', 'orange')
+console.log(aantal_kliks)
+for (let number_button = 0; number_button < buttons.length; i++) {
+    buttons[number_button].addEventListener("click", function () {
+        if (aantal_kliks[number_button] < colors.length){
+            this.style.backgroundColor = colors[aantal_kliks[number_button]];
+        } else{
+            this.style.display = "none";
         }
+        aantal_kliks[number_button]++;
     });
 }
 
-
+console.dir(buttons[0])
 
 
 
